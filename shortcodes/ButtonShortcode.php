9eda61b3b1d1085543ba92dbfe64595e698ad27a
Hotdoy <rouleau.vincent@gmail.com>
2019-11-21 21:46:00 -0500

    completely redone buttons

--- shortcodes/ButtonShortcode.php
+++ shortcodes/ButtonShortcode.php
@@ -10,8 +10,17 @@ class ButtonShortcode extends Shortcode
             $class = $sc->getParameter('class');
             $href = $sc->getParameter('href');
             $i = $sc->getParameter('i');
-            $i_output = $i ? '<i class="material-icons">' . $i . '</i>' : '';
-            return '<div class="button solid"><a href="' . $href . '">' . $sc->getContent() . $i_output . '</a></div>';
+            $i_output = $i ? '<span class="material-icons">' . $i . '</span>' : '';
+            return '<div class="btn btn--solid"><a href="' . $href . '">' . $sc->getContent() . $i_output . '</a></div>';
+        });
+
+        // Duplicate
+        $this->shortcode->getHandlers()->add('btn', function(ShortcodeInterface $sc) {
+            $class = $sc->getParameter('class');
+            $href = $sc->getParameter('href');
+            $i = $sc->getParameter('i');
+            $i_output = $i ? '<span class="material-icons">' . $i . '</span>' : '';
+            return '<div class="btn btn--solid"><a href="' . $href . '">' . $sc->getContent() . $i_output . '</a></div>';
         });
 
         $this->shortcode->getHandlers()->add('btn-link', function(ShortcodeInterface $sc) {
@@ -19,8 +28,18 @@ class ButtonShortcode extends Shortcode
             $href = $sc->getParameter('href');
             $i = $sc->getParameter('i');
             $i_computed = $i ? $i : 'arrow_forward';
-            $i_output = '<i class="material-icons">' . $i_computed . '</i>';
-            return '<div class="button"><a href="' . $href . '">' . $sc->getContent() . $i_output . '</a></div>';
+            $i_output = '<span class="material-icons">' . $i_computed . '</span>';
+            return '<div class="btn btn--link"><a href="' . $href . '">' . $sc->getContent() . $i_output . '</a></div>';
+        });
+
+        // Duplicate
+        $this->shortcode->getHandlers()->add('link', function(ShortcodeInterface $sc) {
+            $class = $sc->getParameter('class');
+            $href = $sc->getParameter('href');
+            $i = $sc->getParameter('i');
+            $i_computed = $i ? $i : 'arrow_forward';
+            $i_output = '<span class="material-icons">' . $i_computed . '</span>';
+            return '<div class="btn btn--link"><a href="' . $href . '">' . $sc->getContent() . $i_output . '</a></div>';
         });
     }
 }
