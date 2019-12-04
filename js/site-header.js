6d76d2fa2875234daa48ccdef5a52ea4cc9f3e10
Hotdoy <rouleau.vincent@gmail.com>
2019-12-04 08:09:48 -0500

    removed all nav-panel options since the header is supposed to be overwritten

--- js/site-header.js
+++ js/site-header.js
@@ -5,26 +5,16 @@
 
 		$('.mobile-nav-open').on('click touch', function () {
 			if ($('.mobile-nav-panel').hasClass('deployed')) {
-				$('.mobile-nav-panel-veil').fadeOut(200);
 				$('.mobile-nav-panel').removeClass('deployed');
 			} else {
 				$('.mobile-nav-panel').addClass('deployed');
-				$('.mobile-nav-panel-veil').fadeIn(200);
 			}
 		});
 
-		$('.mobile-nav-panel-veil, .mobile-nav-close').on('click touch', function () {
+		$('.mobile-nav-close').on('click touch', function () {
 			if ($('.mobile-nav-panel').hasClass('deployed')) {
-				$('.mobile-nav-panel-veil').fadeOut(200);
 				$('.mobile-nav-panel').removeClass('deployed');
-			} else {
-				$('.mobile-nav-panel').addClass('deployed');
-				$('.mobile-nav-panel-veil').fadeIn(200);
 			}
-		});
-
-		$('.mobile-nav-open').on('click touch', function(event){
-		    event.stopPropagation();
 		});
 	}
 
