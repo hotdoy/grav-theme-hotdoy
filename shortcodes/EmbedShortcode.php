f440a6ce5ce46dde445800a91632f1102f62c7ee
Hotdoy <rouleau.vincent@gmail.com>
2019-12-13 13:55:11 -0500

    embed shortcodes now use full urls instead of ids

--- shortcodes/EmbedShortcode.php
+++ shortcodes/EmbedShortcode.php
@@ -6,15 +6,22 @@ class EmbedShortcode extends Shortcode
 {
     public function init()
     {
-        
         $this->shortcode->getHandlers()->add('youtube', function(ShortcodeInterface $sc) {
-        	$id = $sc->getParameter('youtube', $this->getBbCode($sc));
-            return '<div class="embed youtube" data-reveal=""><iframe loading="lazy" src="https://www.youtube.com/embed/'.$id.'?rel=0" frameborder="0" allowfullscreen></iframe></div>';
+            $reg = '/(?:https?:\/{2}(?:(?:www.youtube(?:-nocookie)?\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=))|(?:youtu\.be\/)))([a-zA-Z0-9_-]{11})/';
+            $url = $sc->getParameter('youtube', $this->getBbCode($sc));
+            preg_match($reg, $url, $match);
+            if (isset($match[1])) {
+                return '<div class="embed youtube" data-reveal=""><iframe loading="lazy" src="https://www.youtube.com/embed/'.$match[1].'?rel=0" frameborder="0" allowfullscreen></iframe></div>';
+            }
         });
 
         $this->shortcode->getHandlers()->add('vimeo', function(ShortcodeInterface $sc) {
-        	$id = $sc->getParameter('vimeo', $this->getBbCode($sc));
-            return '<div class="embed vimeo" data-reveal=""><iframe loading="lazy" src="https://player.vimeo.com/video/'.$id.'" frameborder="0" webkitAllowFullScreen mozallowfullscreen></iframe></div>';
+        	$url = $sc->getParameter('vimeo', $this->getBbCode($sc));
+            $id = substr(parse_url($url, PHP_URL_PATH), 1);
+            if (!empty($id)) {
+                return '<div class="embed vimeo" data-reveal=""><iframe loading="lazy" src="https://player.vimeo.com/video/'.$id.'" frameborder="0" webkitAllowFullScreen mozallowfullscreen></iframe></div>';
+            }
+            
         });
     }
 }
