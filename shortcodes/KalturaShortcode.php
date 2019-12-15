044b0c17915d4937bb34b458b1da95ab26c51264
Hotdoy <rouleau.vincent@gmail.com>
2019-12-14 22:34:18 -0500

    allow for custom path

--- shortcodes/KalturaShortcode.php
+++ shortcodes/KalturaShortcode.php
@@ -13,6 +13,7 @@ class KalturaShortcode extends Shortcode
             parse_str($url, $qs);
 
             $output = $this->twig->processTemplate('partials/kalturasc.html.twig', [
+                'path' => $fragments[2],
                 'partner_id' => $fragments[array_search('partner_id', $fragments)+1],
                 'uiconf_id' => $fragments[array_search('uiconf_id', $fragments)+1],
                 'entry_id' => $fragments[array_search('entry_id', $fragments)+1]
