2da657594402dd8df0faee3105de5727e0dad18c
Hotdoy <rouleau.vincent@gmail.com>
2019-12-17 18:27:45 -0500

    starting button sc rework

--- shortcodes/ButtonShortcode.php
+++ shortcodes/ButtonShortcode.php
@@ -6,40 +6,25 @@ class ButtonShortcode extends Shortcode
 {
     public function init()
     {
-        $this->shortcode->getHandlers()->add('btn-solid', function(ShortcodeInterface $sc) {
-            $class = $sc->getParameter('class');
-            $href = $sc->getParameter('href');
-            $i = $sc->getParameter('i');
-            $i_output = $i ? '<span class="material-icons">' . $i . '</span>' : '';
-            return '<div class="btn btn--solid"><a href="' . $href . '">' . $sc->getContent() . $i_output . '</a></div>';
-        });
-
-        // Duplicate
         $this->shortcode->getHandlers()->add('btn', function(ShortcodeInterface $sc) {
-            $class = $sc->getParameter('class');
-            $href = $sc->getParameter('href');
-            $i = $sc->getParameter('i');
-            $i_output = $i ? '<span class="material-icons">' . $i . '</span>' : '';
-            return '<div class="btn btn--solid"><a href="' . $href . '">' . $sc->getContent() . $i_output . '</a></div>';
+            $output = $this->twig->processTemplate('partials/buttonsc.html.twig', [
+                'label' => $sc->getContent(),
+                'href' => $sc->getParameter('href'),
+                'icon' => $sc->getParameter('i'),
+                'class' => 'btn--solid'
+            ]);
+            return $output;
         });
 
-        $this->shortcode->getHandlers()->add('btn-link', function(ShortcodeInterface $sc) {
-            $class = $sc->getParameter('class');
-            $href = $sc->getParameter('href');
-            $i = $sc->getParameter('i');
-            $i_computed = $i ? $i : 'arrow_forward';
-            $i_output = '<span class="material-icons">' . $i_computed . '</span>';
-            return '<div class="btn btn--link"><a href="' . $href . '">' . $sc->getContent() . $i_output . '</a></div>';
-        });
-
-        // Duplicate
         $this->shortcode->getHandlers()->add('link', function(ShortcodeInterface $sc) {
-            $class = $sc->getParameter('class');
-            $href = $sc->getParameter('href');
             $i = $sc->getParameter('i');
-            $i_computed = $i ? $i : 'arrow_forward';
-            $i_output = '<span class="material-icons">' . $i_computed . '</span>';
-            return '<div class="btn btn--link"><a href="' . $href . '">' . $sc->getContent() . $i_output . '</a></div>';
+                $output = $this->twig->processTemplate('partials/buttonsc.html.twig', [
+                'label' => $sc->getContent(),
+                'href' => $sc->getParameter('href'),
+                'icon' => $i ? $i : 'arrow_forward',
+                'class' => 'btn--link'
+            ]);
+            return $output;
         });
     }
 }
