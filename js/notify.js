8b3ce872f46b4fc43206e74eb5e3c51f6f9092a9
Hotdoy <rouleau.vincent@gmail.com>
2020-01-03 11:56:18 -0500

    notify can now remember being accepted and notifications are pretty much done

--- js/notify.js
+++ js/notify.js
@@ -1,27 +1,32 @@
-function notify(message, bg_color, fg_color, bg_src){
-    var done = document.createElement('div');
-    done.setAttribute('class', 'notify__done material-icons');
-    done.innerHTML = 'done';
-    done.addEventListener("click", function () {
-        $(this).parent().addClass('notify--destroy');
-        setTimeout(function(){
-            done.parentNode.remove();
-        }, 1000);
-    });
+function notify(message, bg_color, fg_color, id){
 
-    var notice = document.createElement('div');
-    notice.setAttribute('class', 'notify');
-    if (bg_color) {
-        notice.style.backgroundColor = bg_color;
-    }
-    if (fg_color) {
-        notice.style.color = fg_color;
-    }
-    if (bg_src) {
-        notice.style.backgroundImage = bg_src;
-    }
-    notice.innerHTML = message;
-    notice.appendChild(done);
+    let s = id ? localStorage.getItem('notify-' + id) : null;     
+    if (s == null || Date.now() - s > 21600) {
+        var done = document.createElement('div');
+        done.setAttribute('class', 'notify__done material-icons');
+        done.innerHTML = 'done';
+        done.addEventListener("click", function () {
+            $(this).parent().addClass('notify--destroy');
 
-    $('body').append(notice);
+            if (!!id) {
+               localStorage.setItem('notify-' + id, Date.now()); 
+            }
+            setTimeout(function(){
+                done.parentNode.remove();
+            }, 1000);
+        });
+
+        var notice = document.createElement('div');
+        notice.setAttribute('class', 'notify');
+        if (!!bg_color) {
+            notice.style.backgroundColor = bg_color;
+        }
+        if (!!fg_color) {
+            notice.style.color = fg_color;
+        }
+        notice.innerHTML = message;
+        notice.appendChild(done);
+
+        $('body').append(notice);
+    }
 }
