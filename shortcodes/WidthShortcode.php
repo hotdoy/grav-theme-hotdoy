ca077fe8fe855022cc2dd165890058bde12a8ef6
Hotdoy <rouleau.vincent@gmail.com>
2020-01-11 12:30:59 -0500

    added class support

--- shortcodes/WidthShortcode.php
+++ shortcodes/WidthShortcode.php
@@ -7,15 +7,18 @@ class WidthShortcode extends Shortcode
     public function init()
     {
         $this->shortcode->getHandlers()->add('tight', function(ShortcodeInterface $sc) {
-            return '<div class="tight">' . $sc->getContent() . '</div>';
+            $class = $sc->getParameter('class', $this->getBbCode($sc));
+            return '<div class="tight ' . $class . '">' . $sc->getContent() . '</div>';
         });
 
          $this->shortcode->getHandlers()->add('site', function(ShortcodeInterface $sc) {
-            return '<div class="site">' . $sc->getContent() . '</div>';
+            $class = $sc->getParameter('class', $this->getBbCode($sc));
+            return '<div class="site ' . $class . '">' . $sc->getContent() . '</div>';
         });
 
         $this->shortcode->getHandlers()->add('full', function(ShortcodeInterface $sc) {
-            return '<div class="full">' . $sc->getContent() . '</div>';
+            $class = $sc->getParameter('class', $this->getBbCode($sc));
+            return '<div class="full ' . $class . '">' . $sc->getContent() . '</div>';
         });
     }
 }
