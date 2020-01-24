8c8cd7aa3e03d7087904f4fe8bb4d01e58e3aadf
Hotdoy <rouleau.vincent@gmail.com>
2020-01-24 07:10:30 -0500

    rewrite links.js completelly and added link-rel options in theme admin

--- js/links.js
+++ js/links.js
@@ -2,12 +2,23 @@
 	'use strict';
 
 	var links = function(){
-		let links = $('a[href^=http]');
-		links.each(function(){
-			$(this).attr({
-				target: '_blank',
-				rel: 'noopener'
-			});
+		let links = document.querySelectorAll('a');
+		let strategy = document.body.getAttribute('data-links');
+
+		links.forEach(a => {
+			let href = a.getAttribute('href');
+
+			if (!!href && href.match('^http')) {
+				a.setAttribute('target', '_blank');
+				a.setAttribute('rel', 'noopener');
+			}
+
+			if (!!href && !!strategy && href.match('^/')) {
+				var preLoadLink = document.createElement("link");
+				preLoadLink.rel = strategy;
+    			preLoadLink.href = href;
+				document.head.appendChild(preLoadLink);
+			}	
 		})
 	}
 
