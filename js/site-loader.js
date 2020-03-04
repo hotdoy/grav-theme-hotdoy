70d5ddb44974e5f6f0dcc2d53b398a21df0379ad
Hotdoy <rouleau.vincent@gmail.com>
2020-03-04 07:45:37 -0500

    added demo site-intro with duration controls

--- js/site-loader.js
+++ js/site-loader.js
@@ -1,11 +1,16 @@
 (function () {
 	'use strict';
 	var siteLoader = function(){
-		let loader = document.querySelector('#site-loader');
+		let loader = document.getElementById('loader');
+		let duration = loader.dataset.duration ?? '20';
+		console.log(duration);
 		if (!!loader) {
 			setTimeout(function(){ 
 				loader.classList.add('loaded');
-			}, 20);
+			}, duration);
+			loader.addEventListener('animationend', () =>{
+				loader.parentNode.removeChild(loader);
+			})
 		}
 	}
 	siteLoader();
