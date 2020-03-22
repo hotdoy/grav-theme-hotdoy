a902966675e4c4d4858e8187c47d698bd75ec401
Hotdoy <rouleau.vincent@gmail.com>
2020-03-22 13:51:26 -0400

    removed link rel option from theme and made link rel preconnect default

--- js/links.js
+++ js/links.js
@@ -1,29 +1,25 @@
 (function () {
 	'use strict';
-
 	let links = function(){
+		let b = document.body;
 		let links = document.querySelectorAll('a');
-		let strategy = document.body.getAttribute('data-links');
-
 		links.forEach(a => {
 			let href = a.getAttribute('href');
-
 			if (!!href && href.match('^http')) {
 				a.setAttribute('target', '_blank');
 				a.setAttribute('rel', 'noopener');
 			}
-
 			else if (!!href && href.match('^/')) {
-				if (!!strategy) {
-					let preLoadLink = document.createElement("link");
-					preLoadLink.rel = strategy;
-	    			preLoadLink.href = href;
-					document.head.appendChild(preLoadLink);
-				}
-			}	
+				a.addEventListener('click', function(){
+					b.classList.add('exiting');
+					debug('exiting');
+				});
+				let preLoadLink = document.createElement("link");
+				preLoadLink.rel = 'preconnect';
+    			preLoadLink.href = href;
+				document.head.appendChild(preLoadLink);
+			}
 		})
 	}
-
 	links();
-	
 })();
