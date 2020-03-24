616d0573a98efc7dedfd7ff96788a9ed6cc6d5a0
Hotdoy <rouleau.vincent@gmail.com>
2020-03-23 20:06:37 -0400

    rewrote to be usable in other scope

--- js/links.js
+++ js/links.js
@@ -1,25 +1,22 @@
-(function () {
-	'use strict';
-	let links = function(){
-		let b = document.body;
-		let links = document.querySelectorAll('a');
-		links.forEach(a => {
-			let href = a.getAttribute('href');
-			if (!!href && href.match('^http')) {
-				a.setAttribute('target', '_blank');
-				a.setAttribute('rel', 'noopener');
-			}
-			else if (!!href && href.match('^/')) {
-				a.addEventListener('click', function(){
-					b.classList.add('exiting');
-					debug('exiting');
-				});
-				let preLoadLink = document.createElement("link");
-				preLoadLink.rel = 'preconnect';
-    			preLoadLink.href = href;
-				document.head.appendChild(preLoadLink);
-			}
-		})
-	}
-	links();
-})();
+function links(sel){
+	let body = document.body;
+	let links = document.querySelectorAll(sel);
+	links.forEach(link => {
+		let href = link.getAttribute('href');
+		if (!!href && href.match('^http')) {
+			link.setAttribute('target', '_blank');
+			link.setAttribute('rel', 'noopener');
+		}
+		else if (!!href && href.match('^/')) {
+			link.addEventListener('click', function(){
+				body.classList.add('exiting');
+				debug('exiting');
+			});
+			let preLoadLink = document.createElement("link");
+			preLoadLink.rel = 'preconnect';
+			preLoadLink.href = href;
+			document.head.appendChild(preLoadLink);				
+		}
+	})
+}
+links('a');
