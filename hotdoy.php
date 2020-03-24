d3e84ec13417ca59a0bbaf1507bdd7cc782472df
Hotdoy <rouleau.vincent@gmail.com>
2020-03-24 08:28:49 -0400

    started assets cleanup

--- hotdoy.php
+++ hotdoy.php
@@ -8,9 +8,9 @@ class Hotdoy extends Theme
 	public function onTwigSiteVariables()
     {
         if ($this->isAdmin()) {
-            $this->grav['assets']->add('theme://js/admin-editor-width.js');
-            $this->grav['assets']->add('theme://js/admin-editor-breakpoint.js');
-            $this->grav['assets']->add('theme://js/admin-editor-text.js');
+            $this->grav['assets']->add('theme://js/admin/admin-editor-width.js');
+            $this->grav['assets']->add('theme://js/admin/admin-editor-breakpoint.js');
+            $this->grav['assets']->add('theme://js/admin/admin-editor-text.js');
         }
     }
 }
