b7baa51b267a72a5f851a013a16d17229c11529b
Hotdoy <rouleau.vincent@gmail.com>
2020-03-26 08:15:42 -0400

    rewrote the whole thing

--- js/reveal.js
+++ js/reveal.js
@@ -1,59 +1,37 @@
-(function () {
-	'use strict';
-	let reveal = function(){
-		let elements = document.querySelectorAll('[data-reveal]');
-		let observerConfig = {rootMargin: '-30px', threshold: 0.1};
-		let start = document.querySelector('[data-reveal-start]');
-		let observer = new IntersectionObserver(onIntersection, observerConfig);
-				
-		if (!!start) {
-			start = start.getAttribute('data-reveal-start');
-			elements.forEach(el => {
-				el.classList.add('unrevealed');
-			});
-			setTimeout(function(){
-				elements.forEach(el => {
-					observer.observe(el);
-				});
-			}, start);
-		} else{
-			elements.forEach(el => {
-				observer.observe(el);
-			});
-		}
-
-		function onIntersection(elements) {
-	  		elements.forEach(el => {
-				if (el.intersectionRatio > 0) {
-					let target = el.target;
-					observer.unobserve(target);
-			  		if (target.classList.contains('unrevealed')) {
-			  			let revealClass = target.getAttribute('data-reveal');
-			  			if (revealClass.length) {
-			  				let revealClassArray = revealClass.split(" ");
-			  				el.target.classList.add(...revealClassArray);
-                              let style = getComputedStyle(target);
-                              let animationDuration = parseFloat(style.animationDuration.slice(0,-1));
-                              let animationDelay = parseFloat(style.animationDelay.slice(0,-1));
-                              let totalDelay = (animationDuration + animationDelay) * 1000;
-                              setTimeout(function(){ 
-                                  target.classList.remove('unrevealed');
-                              }, totalDelay);
-
-			  			} else{
-			  				target.classList.add('reveal');
-			  				target.classList.remove('unrevealed');
-			  			}
-			   		}
-				}
-				else{
-					if (!start) {
-						target.classList.add('unrevealed');
-					}
-				}
-			});
+function reveal(e){
+	if (e.classList.contains('unrevealed')) {
+		let revealClass = e.getAttribute('data-reveal') ? e.getAttribute('data-reveal') : 'reveal';
+		if (!!revealClass) {
+			let revealClassArray = revealClass.split(" ");
+			e.classList.add(...revealClassArray);
+		    e.addEventListener('animationend', () =>{
+		    	e.classList.remove('unrevealed');
+		    })
 		}
 	}
+}
 
-	reveal();
-})();
+function initReveal(){
+	let elements = document.querySelectorAll('[data-reveal]');
+	let start = document.querySelector('[data-reveal-start]');
+	start = start ? start.dataset.revealStart : '0';
+	let observer = new IntersectionObserver(onIntersection, {rootMargin: '-30px', threshold: 0.1});	
+	elements.forEach(e => {
+		e.classList.add('unrevealed');
+	});
+	setTimeout(function(){
+		elements.forEach(e => {
+			observer.observe(e);
+		});
+	}, start);
+	function onIntersection(elements) {
+		elements.forEach(e => {
+			if (e.intersectionRatio > 0) {
+				observer.unobserve(e.target);
+				reveal(e.target);
+			}
+		})
+	}
+}
+
+initReveal();
